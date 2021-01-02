5ea7faf8967a128a92cc25e04dc811eacdaf0c8d
Edis Halil <keh_bjk@hotmail.com>
2021-01-02 13:10:35 +0300

    All NuGet packages changed to v3.1.10

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -25,7 +25,7 @@ namespace Blog
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the containers.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDefaultServices(Configuration);
